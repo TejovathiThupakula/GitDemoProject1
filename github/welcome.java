30266da78142004c5a810b4db38ddf67d6acb0b4
Tejovathi <tejut.naidu@gmail.com>
2022-06-06 22:23:01 +0530

    new changes

--- github/welcome.java
+++ github/welcome.java
@@ -2,6 +2,6 @@ package github;
 
 public class welcome {
     public static void main(String[] args) {
-        System.out.println("github learning");
+        System.out.println("welocme to github learning");
     }
 }
